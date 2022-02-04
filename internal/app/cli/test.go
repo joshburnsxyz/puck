177c4212af9a9b687114c10a6818506c9a48ec1b
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-02-04 12:47:27 +1100

    fix test command sig

--- internal/app/cli/test.go
+++ internal/app/cli/test.go
@@ -9,8 +9,8 @@ import (
 func TestCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "test",
-		Aliases: []string{"w"},
-		Args: cobra.ArbitraryArgs,
+		Aliases: []string{"t"},
+		Args: cobra.NoArgs,
 		Short:   "display a test notification",
 		Run: func(cmd *cobra.Command, args []string) {
 				n := notifysend.New("TEST", "this is a test notification")
