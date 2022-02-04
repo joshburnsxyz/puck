b8abf876e08e911fcc96f653522a72ca9ae8f0b4
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-02-04 11:36:12 +1100

    wrapper for notify-send command

---
+++ pkg/notifysend/notifysend.go
@@ -0,0 +1,30 @@
+package notifysend
+
+import (
+	"os/exec"
+	"github.com/rs/zerolog/log"
+	"bytes"
+)
+
+type NotificationCmd struct{
+	Expire: int
+	Urgency: string
+	TitleText: string
+	BodyText: string
+}
+
+func New(title: string, body: string) *NotificationCmd {
+	return &NotificationCmd{
+		Expire: 5000,
+		TitleText: title,
+		BodyText: body
+	}
+}
+
+func (n *NotificationCmd) Send() {
+	cmd := exec.Command("notify-send", "-t", n.Expire, n.TitleText, n.BodyText)
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
